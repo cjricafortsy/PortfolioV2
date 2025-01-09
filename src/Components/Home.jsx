179ffa627f409458c2e0950266cad43fa524b750
bigtimeempirecorp <itassistant@bigtimeempire.com>
2025-01-09 15:26:11 +0800

    Added Responsiveness

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,14 +7,16 @@ import Download from "./Download";
 
 const Home = () => {
   return (
-    <div className="flex flex-row justify-between items-center w-full bg-bgcolor h-[100vh]">
-      <div className="flex flex-row justify-between items-center w-full max-w-[1240px] mx-auto">
-        <div className="flex flex-col items-start w-full">
-          <span className=" text-white text-[2rem] font-bold">Hi!</span>
-          <span className=" text-white text-[3rem] font-bold mt-[-15px]">
+    <div className="flex justify-between items-center w-full bg-bgcolor h-[100vh]">
+      <div className="flex xl:flex-row xl:justify-between items-center w-full max-w-[1240px] 2xl:max-w-[1600px] mx-auto flex-col">
+        <div className="flex flex-col xl:items-start w-full justify-center items-center">
+          <span className=" text-white md:text-[3rem] lg:text-[3rem] xl:text-[2rem] 2xl:text-[2.5rem] font-bold text-[1.3rem]">
+            Hi!
+          </span>
+          <span className=" text-white md:text-[3rem] lg:text-[5rem] xl:text-[3rem] 2xl:text-[3.5rem] font-bold xl:mt-[-15px] text-[1.5rem]">
             I'm Tonghie R. Sy Jr.
           </span>
-          <div className="flex flex-row items-center justify-center text-[2rem] font-bold gap-2">
+          <div className="flex flex-row items-center justify-center md:text-[3rem] 2xl:text-[3.5rem] lg:text-[4.5rem] xl:text-[2rem] font-bold gap-2 text-[1.5rem]">
             <span className="text-white">and I'm a</span>
             <span className="text-textcolor">
               <Typewriter
@@ -32,7 +34,7 @@ const Home = () => {
         <img
           src={Dev}
           alt=""
-          className="Pic flex items-center mt-[5rem] w-[40rem]"
+          className="Pic flex items-center mt-[5rem] md:w-[30rem] lg:w-[50rem] xl:w-[40rem] 2xl:w-[70rem] w-[20rem]"
         />
       </div>
     </div>
--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -10,7 +10,7 @@ const Socials = () => {
       <a
         href="https://www.facebook.com/tonghie.syjr?mibextid=ZbWKwL"
         target="_blank"
-        className="text-textcolor text-[1.8rem] mt-5 rounded-full p-3 border-2 border-textcolor 
+        className="text-textcolor md:text-[2.5rem] lg:text-[4rem] xl:text-[1.8rem] 2xl:text-[2.3rem] mt-5 rounded-full lg:p-5 2xl:p-4 p-3 border-2 border-textcolor 
                     shadow-[0px_0px_8px_2px_rgba(22,90,95,0.5),0px_0px_15px_4px_rgba(22,90,95,0.3)] hover:bg-textcolor hover:text-bgcolor duration-500"
       >
         <span>
@@ -20,7 +20,7 @@ const Socials = () => {
       <a
         href="https://www.instagram.com/cjricafortsy/"
         target="_blank"
-        className="text-textcolor text-[1.8rem] mt-5 rounded-full p-3 border-2 border-textcolor 
+        className="text-textcolor md:text-[2.5rem] lg:text-[4rem] xl:text-[1.8rem] 2xl:text-[2.3rem] mt-5 rounded-full lg:p-5 2xl:p-4 p-3 border-2 border-textcolor 
                     shadow-[0px_0px_8px_2px_rgba(22,90,95,0.5),0px_0px_15px_4px_rgba(22,90,95,0.3)] hover:bg-textcolor hover:text-bgcolor duration-500"
       >
         <span>
@@ -31,7 +31,7 @@ const Socials = () => {
       <a
         href="https://github.com/cjricafortsy"
         target="_blank"
-        className="text-textcolor text-[1.8rem] mt-5 rounded-full p-3 border-2 border-textcolor 
+        className="text-textcolor md:text-[2.5rem] lg:text-[4rem] xl:text-[1.8rem] 2xl:text-[2.3rem] mt-5 rounded-full lg:p-5 2xl:p-4 p-3 border-2 border-textcolor 
                     shadow-[0px_0px_8px_2px_rgba(22,90,95,0.5),0px_0px_15px_4px_rgba(22,90,95,0.3)] hover:bg-textcolor hover:text-bgcolor duration-500"
       >
         <span>
@@ -41,7 +41,7 @@ const Socials = () => {
       <a
         href="https://www.linkedin.com/in/tonghie-sy-61a167280/"
         target="_blank"
-        className="text-textcolor text-[1.8rem] mt-5 rounded-full p-3 border-2 border-textcolor 
+        className="text-textcolor md:text-[2.5rem] lg:text-[4rem] xl:text-[1.8rem] 2xl:text-[2.3rem] mt-5 rounded-full lg:p-5 2xl:p-4 p-3 border-2 border-textcolor 
                     shadow-[0px_0px_8px_2px_rgba(22,90,95,0.5),0px_0px_15px_4px_rgba(22,90,95,0.3)] hover:bg-textcolor hover:text-bgcolor duration-500"
       >
         <span>
