cd94b07632946c22388ffb09dd8a413257132320
bigtimeempirecorp <itassistant@bigtimeempire.com>
2025-01-09 15:44:53 +0800

    Update Responiveness

--- src/Components/Download.jsx
+++ src/Components/Download.jsx
@@ -5,7 +5,7 @@ function Download() {
     <a
       href="/CV.pdf" // Path is relative to the public folder
       download="CV.pdf" // Optional: name the file on download
-      className="text-white md:text-[2rem] xl:text-[1rem] md:py-6 md:px-20 lg:py-[3rem] lg:px-[7rem] mt-5 rounded-full xl:py-4 xl:px-10 py-2 px-5 border-2 border-textcolor 
+      className="text-white md:text-[2rem] xl:text-[1rem] 2xl:text-[1.5rem] md:py-6 md:px-20 lg:py-[3rem] lg:px-[7rem] mt-5 rounded-full xl:py-4 xl:px-10 py-2 px-5 border-2 border-textcolor 
              shadow-[0px_0px_8px_2px_rgba(22,90,95,0.5),0px_0px_15px_4px_rgba(22,90,95,0.3)] 
              bg-textcolor hover:text-bgcolor duration-500 cursor-pointer"
     >
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,15 +8,15 @@ import Download from "./Download";
 const Home = () => {
   return (
     <div className="flex justify-between items-center w-full bg-bgcolor h-[100vh]">
-      <div className="flex xl:flex-row xl:justify-between items-center w-full max-w-[1240px] mx-auto flex-col">
+      <div className="flex xl:flex-row xl:justify-between items-center w-full lg:max-w-[1240px] 2xl:max-w-[1600px] mx-auto flex-col">
         <div className="flex flex-col xl:items-start w-full justify-center items-center">
-          <span className=" text-white md:text-[3rem] lg:text-[3rem] xl:text-[2rem] font-bold text-[1.3rem]">
+          <span className=" text-white md:text-[3rem] lg:text-[3rem] xl:text-[2rem] 2xl:text-[2.5rem] font-bold text-[1.3rem]">
             Hi!
           </span>
-          <span className=" text-white md:text-[3rem] lg:text-[5rem] xl:text-[3rem] font-bold xl:mt-[-15px] text-[1.5rem]">
+          <span className=" text-white md:text-[3rem] lg:text-[5rem] xl:text-[3rem] 2xl:text-[3.5rem] font-bold xl:mt-[-15px] text-[1.5rem]">
             I'm Tonghie R. Sy Jr.
           </span>
-          <div className="flex flex-row items-center justify-center md:text-[3rem] lg:text-[4.5rem] xl:text-[2rem] font-bold gap-2 text-[1.5rem]">
+          <div className="flex flex-row items-center justify-center md:text-[3rem] 2xl:text-[3.5rem] lg:text-[4.5rem] xl:text-[2rem] font-bold gap-2 text-[1.5rem]">
             <span className="text-white">and I'm a</span>
             <span className="text-textcolor">
               <Typewriter
@@ -34,7 +34,7 @@ const Home = () => {
         <img
           src={Dev}
           alt=""
-          className="Pic flex items-center mt-[5rem] md:w-[30rem] lg:w-[50rem] xl:w-[40rem] w-[20rem]"
+          className="Pic flex items-center mt-[5rem] md:w-[30rem] lg:w-[50rem] xl:w-[40rem] 2xl:w-[70rem] w-[20rem]"
         />
       </div>
     </div>
